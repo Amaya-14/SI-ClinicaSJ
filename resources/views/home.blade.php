5ff6621a0e8a9da93b7d86287bc74db37467da06
Daniel Amaya <damayalop@gmail.com>
2022-02-19 20:03:44 -0600

    Primer Commit
    Estructura inicial del proyecto

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+@extends('adminlte::page')
+
+@section('title', 'Home')
+
+@section('css')
+@stop
+
+@section('content_header')
+    <h1>Home</h1>
+@stop
+
+@section('content')
+@stop
+
+@section('js')
+@stop
